da8471014b2b1404dde7cf580c01905b348ebe12
Mbaikop Clinton <mbiakopclinton@gmail.com>
2021-06-11 02:00:41 +0100

    fix: prevent form resumbmition on page refresh. and add: logout button and functionality

--- includes/classes/Post.php
+++ includes/classes/Post.php
@@ -13,6 +13,9 @@ class Post {
     public function submitPost($body, $user_to){
          $body = strip_tags($body);
          $body = mysqli_real_escape_string($this->con, $body);
+         $body = str_replace('\r\n', '\n', $body);
+
+         $body = nl2br($body);
          $check_empty = preg_replace('/\s+/', '', $body);
 
          if($check_empty != ""){
--- index.php
+++ index.php
@@ -10,6 +10,7 @@ include("./includes/classes/Post.php");
 if(isset($_POST['post'])){
     $post = new Post($con, $userLoggedIn);
     $post->submitPost($_POST['post_text'], 'none');
+    header("Location: index.php");
 }
    
 ?>
