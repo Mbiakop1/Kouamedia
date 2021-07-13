776920cd3e638728c3607f58f4d91441678c247a
Mbaikop Clinton <mbiakopclinton@gmail.com>
2021-07-13 02:06:30 +0100

    add style to the messages

--- includes/classes/Message.php
+++ includes/classes/Message.php
@@ -63,7 +63,7 @@ public function getMessages($otherUser){
           $user_from = $row['user_from'];
           $body = $row['body'];
 
-          $div_top = ($user_to == $userLoggedIn) ? "<div class='message' id='green>'" : "<div class='message' id='green'>";
+          $div_top = ($user_to == $userLoggedIn) ? "<div class='message' id='green'>" : "<div class='message' id='blue'>";
           $data = $data . $div_top . $body  . "</div><br><br>";
       }
 
