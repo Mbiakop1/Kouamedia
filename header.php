73c389076bfe98bb58ece20251621089eedb8587
Mbaikop Clinton <mbiakopclinton@gmail.com>
2021-08-28 12:09:38 +0100

    add message dropdown functionality on message icon

--- header.php
+++ header.php
@@ -74,7 +74,7 @@ else {
                         d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5z" />
                 </svg>
             </a>
-            <a href="#">
+            <a href="javascript:void(0);" onclick="getDropdownData('<?php echo $userLoggedIn; ?>', 'message')">
                 <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor"
                     class="bi bi-chat-left-dots" viewBox="0 0 16 16">
                     <path
@@ -117,6 +117,9 @@ else {
                 </svg>
             </a>
         </nav>
+
+        <div class="dropdown_data_window" style="height: 0px; border:none;"></div>
+        <input type="hidden" id="dropdown_data_type" value="">
     </div>
 
     <div class="wrapper">
---
+++ includes/handlers/ajax_load_messages.php
@@ -0,0 +1,11 @@
+<?php
+include_once "../../config/config.php";
+include_once "../classes/User.php";
+include_once "../classes/Message.php";
+
+$limit = 7;  //Number of messages to load
+
+$message = new Message($con, $_REQUEST['userLoggedIn']);
+echo $message->getConvosDropdown($_REQUEST, $limit)
+
+?>
