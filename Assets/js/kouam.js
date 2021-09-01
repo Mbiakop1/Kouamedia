946153faf0bf55555eb1da69926a1f408c7805fb
Mbaikop Clinton <mbiakopclinton@gmail.com>
2021-09-01 12:52:24 +0100

    add increase search input width on focus

--- Assets/js/kouam.js
+++ Assets/js/kouam.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
 
+    $('#search_text_input').focus(function() {
+        if (window.matchMedia("(min-width: 800px)").matches) {
+            $(this).animate({ width: '250px' }, 500);
+        }
+    });
+
+    $('.button_holder').on('click', function() {
+        document.search_form.submit();
+    })
+
     // button for profile post
     $('#submit_profile_post').click(function() {
 
