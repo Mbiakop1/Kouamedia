e35915d74cdd509bf86f4ca58d6b2c11b866a8cd
Mbaikop Clinton <mbiakopclinton@gmail.com>
2021-09-02 14:17:04 +0100

    create the settings_handler.php page

--- settings.php
+++ settings.php
@@ -1,6 +1,6 @@
 <?php
 include "./header.php";
-// include "./includes/settings_handler.php";
+include "./includes/form_handlers/settings_handler.php";
 
 
 ?>
@@ -13,4 +13,28 @@ include "./header.php";
      ?>
     <br>
     <a href="upload.php" class="btn btn-secondary"> Upload new profile picture</a>
+
+    Modify the values and click 'Update details'
+
+    <form action="settings.php" method="POST">
+        Firts Name: <input type="text" name="first_name" value="<?php echo $user['first_name']?>" required> <br>
+        Last Name: <input type="text" name="last_name" value="<?php echo $user['last_name']?>" required> <br>
+        Email: <input type="text" name="email" value="<?php echo $user['email']?>" required><br>
+        <input type="submit" name="update_details" id="save_details" value="Update_details"> <br>
+
+    </form>
+
+    <h4> change password</h4>
+    <form action="settings.php" method="POST">
+        Old password: <input type="password" name="old_password" required> <br>
+        New password: <input type="password" name="new_password_1" required> <br>
+        new possword: <input type="password" name="new_password_2" required><br>
+        <input type="submit" name="update_password" id="save_details" value="Update_password"> <br>
+
+    </form>
+
+    <h4>Close Account</h4>
+    <form action="settings.php" method="POST">
+        <input type="submit" name="close_account" value="Close Account" class="btn btn-danger">
+    </form>
 </div>
