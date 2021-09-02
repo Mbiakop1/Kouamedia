d42b6d8ac1852a899496cde29a1234503f1c9588
Mbaikop Clinton <mbiakopclinton@gmail.com>
2021-09-02 16:27:45 +0100

    add sytle to the settings.php page

--- settings.php
+++ settings.php
@@ -5,14 +5,14 @@ include "./includes/form_handlers/settings_handler.php";
 
 ?>
 
-<div class="main_column column">
+<div id="main_settings" class="main_column column">
     <h4>Account Settings</h4>
 
     <?php
        echo "<img src='" . $user['profile_pic']. "' id='small_profile_pics'>";
      ?>
     <br>
-    <a href="upload.php" class="btn btn-secondary"> Upload new profile picture</a>
+    <a href="upload.php" class="btn btn-primary upload_profile_pic"> Upload new profile picture</a>
 
     Modify the values and click 'Update details'
 
@@ -28,22 +28,26 @@ include "./includes/form_handlers/settings_handler.php";
     ?>
 
     <form action="settings.php" method="POST">
-        Firts Name: <input type="text" name="first_name" value="<?php echo $first_name?>" required> <br>
-        Last Name: <input type="text" name="last_name" value="<?php echo $last_name?>" required> <br>
-        Email: <input type="text" name="email" value="<?php echo $email?>" required><br>
+        Firts Name: <input type="text" name="first_name" value="<?php echo $first_name?>" id="settings_input" required>
+        <br>
+        Last Name: <input type="text" name="last_name" value="<?php echo $last_name?>" id="settings_input" required>
+        <br>
+        Email: <input type="text" name="email" value="<?php echo $email?>" id="settings_input" required><br>
         <?php echo $message; ?>
-        <input type="submit" name="update_details" id="save_details" value="Update_details"> <br>
+        <input type="submit" class="btn btn-warning" name="update_details" id="save_details" value="Update_details">
+        <br>
 
     </form>
 
     <h4> change password</h4>
     <form action="settings.php" method="POST">
-        Old password: <input type="password" name="old_password" required> <br>
-        New password: <input type="password" name="new_password_1" required> <br>
-        New possword: <input type="password" name="new_password_2" required><br>
+        Old password: <input type="password" name="old_password" id="settings_input" required> <br>
+        New password: <input type="password" name="new_password_1" id="settings_input" required> <br>
+        New possword: <input type="password" name="new_password_2" id="settings_input" required><br>
         <?php echo $password_message; ?> <br>
 
-        <input type="submit" name="update_password" id="save_details" value="Update_password"> <br>
+        <input type="submit" class="btn btn-warning" name="update_password" id="save_details" value="Update_password">
+        <br>
 
     </form>
 
