737e925ea6ab7ddb42b7a921c1e1f0ee08b51bb8
Mbaikop Clinton <mbiakopclinton@gmail.com>
2021-09-02 15:54:50 +0100

    add close account functionality in close_account.php page

--- close_account.php
+++ close_account.php
@@ -1,7 +1,16 @@
 <?php
-
 include "./header.php";
 
+if(isset($_POST['cancel'])){
+    header("Location: settings.php");
+
+}
+
+if(isset($_POST['close_account'])){
+    $close_query = mysqli_query($con, "UPDATE users SET user_closed='yes' WHERE username='$userLoggedIn'");
+    session_destroy();
+    header("Location: register.php");
+}
 
 ?>
 
@@ -12,4 +21,10 @@ include "./header.php";
     Are you sure you want to close your account?<br><br>
     Closing your account will hide your profile and all your activity from other users.<br><br>
     You can re-open your account at any time by simply logging in.<br><br>
+
+    <form action="close_account.php" method="POST">
+        <input class="btn btn-danger" type="submit" name="close_account" id="close_account" value="Yes! Close it!">
+        <input class="btn btn-primary" type="submit" name="cancel" id="cancel" value="Cancel">
+
+    </form>
 </div>
