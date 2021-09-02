4caf9911d29763cf17dbc286c339c3e445413f64
Mbaikop Clinton <mbiakopclinton@gmail.com>
2021-09-02 12:07:24 +0100

    add search results on search.php page.

--- search.php
+++ search.php
@@ -28,7 +28,7 @@ if(isset($_GET['type'])){
 // if query contains an underscore, assume user is searching for username
 
 if($type == "username"){
-    $usersReturnedQuery = mysqli_query($con, "SELECT * FROM users WHERE (username LIKE '$query%' OR username LIKE '%$query') AND user_closed='no' limit 8 ");
+    $usersReturnedQuery = mysqli_query($con, "SELECT * FROM users WHERE (username LIKE '$query%' OR username LIKE '%$query') AND user_closed='no'");
 
 }//if there are two, assume they are firts and last name respectively
 else {
@@ -51,7 +51,67 @@ else {
 
     $usersReturnedQuery = mysqli_query($con, "SELECT * FROM users WHERE ((first_name LIKE '$name[0]%' OR first_name LIKE '%$name[0]') OR (last_name LIKE '$name[0]%' OR last_name LIKE '%$name[0]')) AND user_closed='no'");
 }
+
 }
+//  check if results were found 
+
+if(mysqli_num_rows($usersReturnedQuery) == 0){
+    echo "We cant find anyone with a " . $type . " like: " .$query;
+} else {
+    echo mysqli_num_rows($usersReturnedQuery) . " results found: <br><br>";
+} 
+
+echo "<p id='grey'>Try searching for:</p>";
+echo "<a href='search.php?q=" . $query. "&type=name'>Names</a>,  <a href='search.php?q=" . $query ."&type=username'>Usernames</a><br><br><hr>";
+
+while($row = mysqli_fetch_array($usersReturnedQuery)){
+    $user_obj = new User($con, $user['username']);
+
+    $button = "";
+    $mutual_friends = "";
+
+    if($user['username'] != $row['username']){
+        // generate button depending on friendship status
+        if($user_obj->isFriend($row['username'])){
+            $button = "<input type='subit' name='" . $row['username']. "' class='btn btn-danger' value='Remove Friend'>";
+        } else if($user_obj->didReceiveRequest($row['username'])){
+            $button = "<input type='subit' name='" . $row['username']. "' class='btn btn-warning' value='Respond to request'>";
+             
+        } else if($user_obj->didSendRequest($row['username'])){
+            $button = "<input class='btn-default' value='Request Sent'>";
+        } else {
+            $button = "<input type='subit' name='" . $row['username']. "' class='btn btn-success' value='Add friend'>";
+            
+        }
+
+        $mutual_friends = $user_obj->getMutualFriends($row['username']) . " friends in common";
+
+
+        // button forms
+    }
+
+    echo "<div class='search_result'>
+            <div class='searchPageFriendsButton'>
+                <form action='' method='POST'>
+                    " . $button . "
+                    <br>
+                </form>
+            </div>
+
+            <div class='result_profile_pic'>
+              <a href='" . $row['username'] ."'><img src='" . $row['profile_pic'] ."' stlye='height: 100px;'></a>
+            </div>
+              <a href='" . $row['username'] ."'>" . $row['first_name']. " " . $row['last_name']. "
+               <p id='grey'> ". $row['username']."</p>
+              </a>
+              <br>
+              " . $mutual_friends . "<br>
+         </div>
+         <hr>";
+
+
+
+} // end of while loop
  
 
 
