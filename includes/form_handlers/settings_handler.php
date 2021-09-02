310349dffb7188f393f4872abae44424b21d732b
Mbaikop Clinton <mbiakopclinton@gmail.com>
2021-09-02 14:47:23 +0100

    add update details functionality on settings.php page

--- includes/form_handlers/settings_handler.php
+++ includes/form_handlers/settings_handler.php
@@ -1,15 +1,21 @@
 <?php
 
 if(isset($_POST['update_details'])){
-    $first_name = $_POST['fisrt_name'];
+    $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
     $email = $_POST['email'];
 
 
     $email_check = mysqli_query($con, "SELECT * FROM  users WHERE email='$email'");
     $row = mysqli_fetch_array($email_check);
-    $mached_user = $row['username'];
-    if($mached_user == "" || $mached_user == $userLoggedIn){
+    
+    if(isset($row['username'])){
+        $matched_user = $row['username'];
+    } else {
+         $matched_user = "";
+    }
+   
+    if($matched_user == "" || $matched_user == $userLoggedIn){
         $message = "Details updated!<br><br>";
 
         $query = mysqli_query($con, "UPDATE users SET first_name='$first_name', last_name='$last_name', email='$email' WHERE username='$userLoggedIn'");
--- settings.php
+++ settings.php
@@ -16,10 +16,22 @@ include "./includes/form_handlers/settings_handler.php";
 
     Modify the values and click 'Update details'
 
+    <?php
+    
+    $user_data_query = mysqli_query($con, "SELECT * FROM users WHERE username='$userLoggedIn'");
+    $row = mysqli_fetch_array($user_data_query);
+
+    $first_name = $row['first_name'];
+    $last_name = $row['last_name'];
+    $email = $row['email'];
+
+    ?>
+
     <form action="settings.php" method="POST">
-        Firts Name: <input type="text" name="first_name" value="<?php echo $user['first_name']?>" required> <br>
-        Last Name: <input type="text" name="last_name" value="<?php echo $user['last_name']?>" required> <br>
-        Email: <input type="text" name="email" value="<?php echo $user['email']?>" required><br>
+        Firts Name: <input type="text" name="first_name" value="<?php echo $first_name?>" required> <br>
+        Last Name: <input type="text" name="last_name" value="<?php echo $last_name?>" required> <br>
+        Email: <input type="text" name="email" value="<?php echo $email?>" required><br>
+        <?php echo $message; ?>
         <input type="submit" name="update_details" id="save_details" value="Update_details"> <br>
 
     </form>
